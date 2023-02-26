26338ba52b8d832667ee87d3bc02806471b9bf79
Hrithik verma <hrithikverma2811@gmail.com>
2023-02-26 15:43:12 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require("express")
+var cheerio = require("cheerio");
+const { get } = require("mongoose");
+var cors = require("cors")
+
+var app = express()
+
+app.use(cors());
+ 
+
+async function getPrice(scripName){
+    var url = `https://www.google.com/finance/quote/${scripName}:nse`
+
+    var response = await fetch(url);
+    var data = await response.text();
+    var $ = cheerio.load(data);
+    var price = $(".YMlKec.fxKbKc").html()
+    price = price.replace(price[0], "")
+    price = price.replace(/,/g, "")
+    price = parseFloat(price)
+    return price;
+}
+
+app.get("/price/:scripName",async (req,res)=>{
+    // var price = await getPrice(['sbicard','sbin','rblbank']);
+    var price = await getPrice(req.params.scripName);
+    // console.log(price);
+    // res.send("Done")
+    var date = new Date()
+    console.log(date.getHours().toString() +':'+ date.getMinutes().toString() +':'+ date.getSeconds().toString());
+    res.json({name: req.params.scripName.toUpperCase(),
+    price : price})
+    
+})
+
+
+app.listen(80,()=>{
+    console.log("server started!")
+})
+
+
+
